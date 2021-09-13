25360f9f11a8eb998839236c6e1f923a215c8594
sayantan3 <sayantand975@gmail.com>
2021-09-13 19:58:00 +0530

    update and correction

--- auto_easy.py
+++ auto_easy.py
@@ -7,27 +7,38 @@
 
 import os
 import time
-import sys
-from datetime import datetime
-from sys import argv
 
 
 def create_c():
-    os.chdir("/Users/sayantandas/Desktop/test/")  # working directory for test file
-    file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.c')
-    with open(file_name, 'w') as f:
+    i = os.chdir("/Users/sayantandas/fun/automation/test/")
+    file_name = time.strftime("%Y-%m-%d-%H-%M-%S.c")
+    with open((file_name), 'w+') as f:
         f.write('created a new c file!')
 
 def create_cc():
-    os.chdir("/Users/sayantandas/Desktop/test/")  # working directory for test file
-    file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.cc')
-    with open(file_name, 'w') as f:
+    i = os.chdir("/Users/sayantandas/fun/automation/test/")
+    file_name = time.strftime("%Y-%m-%d-%H-%M-%S.cc")
+    with open(file_name, 'w+') as f:
         f.write('created a new cc file!')
   
 def create_py():
-    os.chdir("/Users/sayantandas/Desktop/test/")  # working directory for test file
-    file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.py')
-    with open(file_name, 'w') as f:
+    i = os.chdir("/Users/sayantandas/fun/automation/test/")
+    file_name = time.strftime("%Y-%m-%d-%H-%M-%S.py")
+    with open(file_name, 'w+') as f:
         f.write('created a new py file!')
 
+def default():
+    return "Please choose correct input 1 for c, 2 for c++, 3 for python"
 
+print("give i/p 1 for c, 2 for c++, 3 for python")
+
+i = int(input("please enter\n"))
+
+if i == 1:
+    print(create_c())
+elif i == 2:
+    print(create_cc())
+elif i == 3:
+    print(create_py())
+else:
+    print("please give i/p between 1-3 next time")
